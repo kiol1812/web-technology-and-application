cbbbf5b9047b575b98b51992b7765fd20fd5d171
kiol1812 <kioly.vikerg0412@gmail.com>
2024-12-23 17:30:12 +0800

    ✨ W15

---
+++ example/MVC/ch11a/sln41143264ShoppingCar/prj41143264ShoppingCar/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using prj41143264ShoppingCar.Models;
+
+namespace prj41143264ShoppingCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        //建立可存取dbShoppingCar.mdf 資料庫的dbShoppingCarEntities 類別物件db
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            //取得所有產品放入products
+            var products = db.tProduct.OrderByDescending(m => m.fId).ToList();
+            return View(products);
+        }
+
+        //Get: Home/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Post: Home/Login
+        [HttpPost]
+        public ActionResult Login(string fUserId, string fPwd)
+        {
+            // 依帳密取得會員並指定給member
+            var member = db.tMember
+                .Where(m => m.fUserId == fUserId && m.fPwd == fPwd)
+                .FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if (member == null)
+            {
+                ViewBag.Message = "帳密錯誤，登入失敗";
+                return View();
+            }
+            //使用Session變數記錄歡迎詞
+            Session["WelCome"] = member.fName + "歡迎光臨";
+            FormsAuthentication.RedirectFromLoginPage(fUserId, true);
+            return RedirectToAction("Index", "Member");
+        }
+
+        //Get:Home/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //Post:Home/Register
+        [HttpPost]
+        public ActionResult Register(tMember pMember)
+        {
+            //若模型沒有通過驗證則顯示目前的View
+            if (ModelState.IsValid == false)
+            {
+                return View();
+            }
+            // 依帳號取得會員並指定給member
+            var member = db.tMember
+                .Where(m => m.fUserId == pMember.fUserId)
+                .FirstOrDefault();
+            //若member為null，表示會員未註冊
+            if (member == null)
+            {
+                //將會員記錄新增到tMember資料表
+                db.tMember.Add(pMember);
+                db.SaveChanges();
+                //執行Home控制器的Login動作方法
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "此帳號己有人使用，註冊失敗";
+            return View();
+        }
+
+    }
+}
